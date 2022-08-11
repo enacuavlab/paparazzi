90d7e821dbeb765772547878846d29eef9116a73
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2022-08-11 18:11:45 +0200

    clean unused files

--- sw/ground_segment/python/mark_tracker/mark_tracker.py
+++ sw/ground_segment/python/mark_tracker/mark_tracker.py
@@ -2,14 +2,12 @@
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
 import mark_tracker_core
-import mark_tracker_new
-sys.path.append("/home/vladborza/paparazzi/sw/ground_segment/python/mark_tracker")
 import argparse
 
 def main():
   app = QtWidgets.QApplication(sys.argv)
   MainWindow = QtWidgets.QMainWindow()
-  tracker = mark_tracker_new.Tracker(verbose=True)
+  tracker = mark_tracker_core.Tracker(verbose=True)
   app.aboutToQuit.connect(tracker.closing)
   tracker.setupUi(MainWindow)
   tracker.built()
@@ -17,5 +15,5 @@ def main():
   sys.exit(app.exec_())
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description="Interface Mark tracking at IMAV2019")
+  parser = argparse.ArgumentParser(description="Interface Mark tracking at IMAV2022")
   main()
