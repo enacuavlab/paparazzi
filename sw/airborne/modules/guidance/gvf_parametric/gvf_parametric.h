e52850174ebf9dd3124d2aba4ff9475d66032611
Mael Feurgard <mael.feurgard@epfl.ch>
2023-03-28 10:24:07 +0200

    Added 3D sinusoid + affine transform

--- sw/airborne/modules/guidance/gvf_parametric/gvf_parametric.h
+++ sw/airborne/modules/guidance/gvf_parametric/gvf_parametric.h
@@ -79,14 +79,17 @@
 #define GVF_PARAMETRIC_COORDINATION_MAX_NEIGHBORS 4
 #endif
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "modules/guidance/gvf_parametric/trajectories/gvf_parametric_3d_ellipse.h"
-#include "modules/guidance/gvf_parametric/trajectories/gvf_parametric_3d_lissajous.h"
-#include "modules/guidance/gvf_parametric/trajectories/gvf_parametric_2d_trefoil.h"
+//#include "modules/guidance/gvf_parametric/trajectories/gvf_parametric_3d_ellipse.h"
+//#include "modules/guidance/gvf_parametric/trajectories/gvf_parametric_3d_lissajous.h"
+//#include "modules/guidance/gvf_parametric/trajectories/gvf_parametric_2d_trefoil.h"
+
+#include "trajectories/gvf_parametric_3d_ellipse.h"
+#include "trajectories/gvf_parametric_3d_lissajous.h"
+#include "trajectories/gvf_parametric_2d_trefoil.h"
 
 /** @typedef gvf_parametric_con
 * @brief Control parameters for the GVF_PARAMETRIC
@@ -140,6 +143,7 @@ enum trajectories_parametric {
   ELLIPSE_3D = 1,
   LISSAJOUS_3D = 2,
   TORUS_3D_SURFACE = 3,
+  SINUS_3D = 4,
   NONE_PARAMETRIC = 255,
 };
 
@@ -156,8 +160,100 @@ extern void gvf_parametric_init(void);
 
 // Control functions
 extern void gvf_parametric_set_direction(int8_t);
+
+/**
+ * @brief Define the affine component of the transformation to apply to the current trajectory
+ * 
+ * @param x x-axis shift
+ * @param y y-axis shift
+ * @param z z-axis shift
+ */
+void gvf_parametric_set_offset(float x, float y, float z);
+
+
+/**
+ * @brief Like gvf_parametric_set_offset, but uses a waypoint instead
+ * 
+ * @param wp Waypoint's ID
+ */
+void gvf_parametric_set_offset_wp(uint8_t wp);
+
+/**
+ * @brief Define the rotational component (through its Euler's angles) of the transformation to apply to the current trajectory
+ * 
+ * @param rz First rotation around z-axis (in degrees)
+ * @param ry Rotation around y-axis (in degrees)
+ * @param rzbis Second rotation around z-axis (in degrees)
+ * 
+ * The resulting rotation is:
+ * R_z(rzbis) ∘ R_y(ry) ∘ R_z(rz)
+ */
+void gvf_paremetric_set_euler_rot(float rz, float ry, float rzbis);
+
+/**
+ * @brief Define the rotational component (through its Cardan's angles) of the transformation to apply to the current trajectory
+ * 
+ * @param rx Rotation around x-axis (in degrees)
+ * @param ry Rotation around y-axis (in degrees)
+ * @param rz Rotation around z-axis (in degrees)
+ * 
+ * The resulting rotation is:
+ * R_z(rz) ∘ R_y(ry) ∘ R_x(rx)
+ */
+void gvf_paremetric_set_cardan_rot(float rx, float ry, float rz);
+
+
+/**
+ * @brief Set the rotation such that the x-axis is aligned with the vector from wp1 to wp2
+ * 
+ * @param wp1 Source waypoint ID
+ * @param wp2 Destination waypoint ID
+ */
+void gvf_parametric_set_wps_rot(uint8_t wp1, uint8_t wp2);
+
+/**
+ * @brief Like gvf_parametric_set_wps_rot, but consider wp1 = (0,0,0) (and take only wp2 as argument)
+ * 
+ * @param wp Direction's waypoint ID
+ */
+void gvf_parametric_set_wp_rot(uint8_t wp);
+
+/**
+ * @brief Define the affine transform through translation and Cardan's angles
+ * 
+ * @param x x-axis shift
+ * @param y y-axis shift
+ * @param z z-axis shift
+ * @param rx Rotation around x-axis (in degrees)
+ * @param ry Rotation around y-axis (in degrees)
+ * @param rz Rotation around z-axis (in degrees)
+ * 
+ * @see gvf_paremetric_set_cardan_rot
+ */
+void gvf_parametric_set_affine_tr(float x, float y, float z, float rx, float ry, float rz);
+
+
+/**
+ * @brief Like gvf_parametric_set_affine_tr, but set the offset through a waypoint
+ * 
+ * @param wp Origin waypoint ID 
+ * @param rx Rotation around x-axis (in degrees)
+ * @param ry Rotation around y-axis (in degrees)
+ * @param rz Rotation around z-axis (in degrees)
+ */
+void gvf_parametric_set_affine_tr_wp(uint8_t wp, float rx, float ry, float rz);
+
+/**
+ * @brief Define the affine transform through the line passing through two waypoints,
+ * such that the origin is at wp1, and the x-axis go through wp2
+ * 
+ * @param wp1 ID of the 'origin' waypoint
+ * @param wp2 ID of the 'direction' waypoint
+ */
+void gvf_parametric_set_affine_tr_wps(uint8_t wp1, uint8_t wp2);
+
 extern void gvf_parametric_control_2D(float, float, float, float, float, float, float, float);
-extern void gvf_parametric_control_3D(float, float, float, float, float, float, float, float, float,
+extern void gvf_parametric_control_3d(float, float, float, float, float, float, float, float, float,
                                       float, float, float);
 
 // Coordination functions
@@ -174,15 +270,21 @@ extern bool gvf_parametric_2D_trefoil_XY(float, float, float, float, float, floa
 extern bool gvf_parametric_2D_trefoil_wp(uint8_t, float, float, float, float, float);
 
 // 3D Ellipse
-extern bool gvf_parametric_3D_ellipse_XYZ(float, float, float, float, float, float);
-extern bool gvf_parametric_3D_ellipse_wp(uint8_t, float, float, float, float);
-extern bool gvf_parametric_3D_ellipse_wp_delta(uint8_t, float, float, float, float);
+extern bool gvf_parametric_3d_ellipse_XYZ(float, float, float, float, float, float);
+extern bool gvf_parametric_3d_ellipse_wp(uint8_t, float, float, float, float);
+extern bool gvf_parametric_3d_ellipse_wp_delta(uint8_t, float, float, float, float);
 
 // 3D Lissajous
-extern bool gvf_parametric_3D_lissajous_XYZ(float, float, float, float, float, float, float, float, float, float, float,
+extern bool gvf_parametric_3d_lissajous_XYZ(float, float, float, float, float, float, float, float, float, float, float,
     float, float);
-extern bool gvf_parametric_3D_lissajous_wp_center(uint8_t, float, float, float, float, float, float, float, float,
+extern bool gvf_parametric_3d_lissajous_wp_center(uint8_t, float, float, float, float, float, float, float, float,
     float, float, float);
+
+// 3D Sinusoid
+bool gvf_parametric_3d_sin(float ay, float freq_y, float az, float freq_z, float phase);
+bool gvf_parametric_3d_sin_XYZa(float xo, float yo, float zo, float alpha,
+                                float ay, float freq_y, float az, float freq_z, float phase);
+
 
 #ifdef __cplusplus
 }
