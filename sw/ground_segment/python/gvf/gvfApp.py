3a1e3f63f9e0de9cf5189a4719ff45d4d50be51d
Mael Feurgard <mael.feurgard@epfl.ch>
2023-03-31 16:27:36 +0200

    Use `argparse` for entry point

--- sw/ground_segment/python/gvf/gvfApp.py
+++ sw/ground_segment/python/gvf/gvfApp.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 
-import sys
 import wx
 #from gvfframe import GVFFrame 
 from modernGVFframe import GVFFrame
 import argparse
 
 class MessagesApp(wx.App):
-    def __init__(self, wtf, ac_id):
+    def __init__(self, wtf, ac_id, dims=2):
         self.ac_id = ac_id
+        self.dims = dims
         wx.App.__init__(self, wtf)
 
     def OnInit(self):
-        self.main = GVFFrame(self.ac_id)
+        self.main = GVFFrame(self.ac_id,self.dims)
 
         self.main.Show()
         self.SetTopWindow(self.main)
         return True
 
 def main():
-    if len(sys.argv) != 2:
-        print ("Usage: gvfApp id_aircraft")
-        return
-    id_ac = int(sys.argv[1])
-    application = MessagesApp(0, id_ac)
+    parser = argparse.ArgumentParser("gvfApp", description="Python App for visualization GVF while PaparazziUAV is running")
+    parser.add_argument('id_aircraft',type=int,help="ID of the aircraft one wants to plot its GVF")
+    parser.add_argument('-d','--dim',dest='dim',choices=['2','3'],default=2,
+                        help="Dimension asked for plotting (either 2 or 3, default is 2)")
+    
+    args = parser.parse_args()
+    id_ac = args.id_aircraft
+    dims = int(args.dim)
+    application = MessagesApp(0, id_ac, dims)
     application.MainLoop()
 
 if __name__ == '__main__':
