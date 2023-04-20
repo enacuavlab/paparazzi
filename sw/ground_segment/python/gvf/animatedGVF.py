d8030c4c9b02966dacfaccbaa414755cc0ac5733
Mael Feurgard <mael.feurgard@epfl.ch>
2023-04-20 08:55:14 +0200

    A Matplotlib only, dynamic GVF plotting tool (WIP)

---
+++ sw/ground_segment/python/gvf/animatedGVF.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+
+from animatedGVF.animation import Trajectory3DMap
+
+def main():
+    parser = argparse.ArgumentParser("AnimatedGVF", description="A Matplotlib based dynamic visualisation tool\
+        for GVF-based PaparazziUAV flights. Show a 3D plot and the 3 'canonical' projections (XY,XZ,YZ).")
+    
+    parser.add_argument('ids',nargs='+',
+                        help="List of aircraft ids' to track")
+    parser.add_argument('--name',type=str,dest='name',default='GVF_collector',
+                        help="Name for the underlying Ivy interface collecting messages. Default is 'GVF_collector'")
+    parser.add_argument('-w','--w-dist',dest='w_dist',type=float,default=400,
+                        help="Range (positive) around the carrot point for plotting the trajectory.\
+                            May be ignored for closed trajectory (plot everything instead).\
+                            For non-parametric ones (no carrot), some arbitrary reference point is used...\
+                            Default is 400.")
+    
+    parser.add_argument('-gvf',dest='gvf',default=False,nargs='?',
+                        help="Display the Guiding Vector field around the aircrafts (only in the planar projections).\
+                        With no arguments, enable it with range 300m. Otherwise, enable it with specified range.\
+                        By default: enabled if only one aircraft is tracked, disabled otherwise.")
+    
+    parser.add_argument('--no-gvf',dest='no_gvf',action='store_true',default=False,
+                        help="Disable GVF plotting entierly. Superseed the 'gvf' flag.")
+    
+    parser.add_argument('-r','--resolution',dest='resolution',default=10,type=int,
+                        help="Number of datapoints per dimension used when computing the GVF.\
+                        For computing trajectories, the number of datapoints is 10 times the resolution.\
+                        Default is 10.")
+    
+    parser.add_argument('-hs','--history-size',dest='history_size',type=int,default=100,
+                        help="Number of previous positions shown (in frames) kept while plotting.\
+                        If set to 0 or 1, no history is shown. Default is 100.")
+    
+    parser.add_argument('-fps',dest='fps',type=int,default=30,
+                        help="Number of frames per second wanted for the animation\
+                        (i.e. number of look-up of the current state per second, the state being automatically updated\
+                        by the Ivy interface manager.) Default is 30.")
+    
+    args = parser.parse_args()
+    
+    ac_ids = [int(i) for i in args.ids]
+    if args.no_gvf:
+        show_field = False
+        gvf_dist = 300.
+    else:
+        if args.gvf is False:
+            show_field = len(ac_ids) == 1
+        else:
+            show_field = True
+        
+        if args.gvf is not None and args.gvf is not False:
+            gvf_dist = float(args.gvf)
+        else:
+            gvf_dist = 300.
+    
+    map = Trajectory3DMap(ac_ids,args.name,args.w_dist,show_field,gvf_dist,
+                          args.resolution,args.history_size,args.fps)
+    
+    map.start()
+    
+if __name__ == "__main__":
+    main()
+            
+    
+    
+    
+    
---
+++ sw/ground_segment/python/gvf/animatedGVF/pprzInterface.py
@@ -0,0 +1,182 @@
+import typing
+import inspect
+from dataclasses import dataclass
+from time import sleep
+import os 
+
+import numpy as np
+
+from trajectories import GVF, GVF_PARAMETRIC
+from trajectories.gvf_trajectories import Trajectory, LineTrajectory,SurfaceTrajectory
+
+from pprzlink.generated import telemetry,ground
+from pprzlink.message import PprzMessage
+from pprzlink.ivy import IvyMessagesInterface
+
+import sys
+from os import path, getenv
+PPRZ_HOME = getenv("PAPARAZZI_HOME", path.normpath(
+    path.join(path.dirname(path.abspath(__file__)), '../../../../../')))
+PPRZ_SRC = getenv("PAPARAZZI_SRC", path.normpath(
+    path.join(path.dirname(path.abspath(__file__)), '../../../../../')))
+sys.path.append(PPRZ_HOME + "/var/lib/python")
+sys.path.append(PPRZ_SRC + "/sw/lib/python")
+
+from settings import PprzSettingsManager
+from pprz_connect import PprzConnect,PprzConfig
+
+#################### Meta code to import all known trajectories ####################
+
+
+def __get_GVF_traj_map() -> typing.Dict[int, object]:
+    classes = list(filter(lambda t: inspect.isclass(t[1]) 
+                          and issubclass(t[1],(LineTrajectory,SurfaceTrajectory))
+                          and not inspect.isabstract(t[1]), inspect.getmembers(GVF)))
+    output = dict()
+    for (n, o) in classes:
+        try:
+            output[o.class_id()] = o
+        except Exception as e:
+            print(f"Error with class {n}:\n{o}")
+            raise e
+    return output
+
+
+GVF_traj_map = __get_GVF_traj_map()
+
+
+def __get_GVF_PARAMETRIC_traj_map() -> typing.Dict[int, object]:
+    classes = list(filter(lambda t: inspect.isclass(t[1]) 
+                          and issubclass(t[1],(LineTrajectory,SurfaceTrajectory))
+                          and not inspect.isabstract(t[1]), inspect.getmembers(GVF_PARAMETRIC)))
+    output = dict()
+    for (n, o) in classes:
+        try:
+            output[o.class_id()] = o
+        except Exception as e:
+            print(f"Error with class {n}:\n{o}")
+            raise e
+    return output
+
+
+GVF_PARAMETRIC_traj_map = __get_GVF_PARAMETRIC_traj_map()
+
+#################### Aircraft data container ####################
+
+@dataclass
+class Aircraft():
+    id:int
+    settingManager:PprzSettingsManager
+    
+    attitude:typing.Optional[telemetry.PprzMessage_ATTITUDE] = None
+    navigation:typing.Optional[telemetry.PprzMessage_NAVIGATION] = None
+    ground_data:typing.Optional[ground.PprzMessage_FLIGHT_PARAM] = None
+    gps:typing.Optional[telemetry.PprzMessage_GPS] = None
+    gvf:typing.Optional[telemetry.PprzMessage_GVF] = None
+    gvf_parametric:typing.Optional[telemetry.PprzMessage_GVF_PARAMETRIC] = None
+    config:typing.Optional[PprzConfig] = None
+    
+    @property
+    def XYZ(self) -> np.ndarray:
+        if self.gps is None:
+            return np.zeros(3)
+        
+        if self.navigation is None:
+            return np.zeros(3)
+        
+        return np.array([self.navigation.pos_x_,self.navigation.pos_y_, self.gps.alt_ / 1000])
+    
+    
+    @property
+    def trajectory(self) -> Trajectory:
+        if self.gvf_parametric is None and self.gvf is None:
+            return None
+        
+        if self.gvf_parametric is None:
+            return GVF_traj_map[self.gvf.traj_].from_message(self.gvf)
+        
+        else:
+            return GVF_PARAMETRIC_traj_map[self.gvf_parametric.traj_].from_message(self.gvf_parametric)
+    
+    @property
+    def w(self) -> float:
+        if self.gvf_parametric is None:
+            return None
+        else:
+            return self.gvf_parametric.w_
+        
+        
+
+#################### Aircraft data collector ####################
+
+class AC_DataCollector():
+    def __init__(self,ac_id_list:typing.List[int],name:str="GVF_collector",setup:bool=True) -> None:
+        
+        # Vehicle variables
+        self.ac_ids:typing.List[int] = ac_id_list
+        self.ac_dict:typing.Dict[int,Aircraft] = dict()
+        
+        # Ivy
+        self._ivy_interface = IvyMessagesInterface(name)
+        self._pprz_connect = PprzConnect(ivy=self._ivy_interface)
+        
+        # Setup
+        if setup:
+            self.setup()
+        
+    def setup(self) -> None:
+        """
+        **Wait** until aircrafts' config data are gathered (and setup associated callbacks)
+        """
+        for i in self.ac_ids:
+            success = False
+            while not success:
+                try:
+                    self._pprz_connect.get_config(i)
+                    conf_msg:PprzConfig = self._pprz_connect.conf_by_id(i)
+                    success = True
+                except KeyError:
+                    print(f"Waiting for aircraft with id {i} ...")
+                    sleep(1.)
+                
+            settings_path = os.path.normpath(conf_msg.settings)
+            self.ac_dict[i] = Aircraft(i,PprzSettingsManager(settings_path,i,self._ivy_interface))
+            self.ac_dict[i].config = conf_msg
+            
+        
+        # bind to ATTITUDE message
+        def attitude_cb(ac_id, msg:telemetry.PprzMessage_ATTITUDE):
+            if ac_id in self.ac_ids:# and msg.name == "ATTITUDE":
+                ac = self.ac_dict[ac_id]
+                ac.attitude = msg
+        self._ivy_interface.subscribe(attitude_cb, telemetry.PprzMessage_ATTITUDE())
+            
+        # bind to NAVIGATION message
+        def nav_cb(ac_id, msg:telemetry.PprzMessage_NAVIGATION):
+            if ac_id in self.ac_ids:# and msg.name == "NAVIGATION":
+                ac = self.ac_dict[ac_id]
+                ac.navigation = msg
+        self._ivy_interface.subscribe(nav_cb, telemetry.PprzMessage_NAVIGATION())
+
+        # bind to GPS message
+        def gps_cb(ac_id, msg:telemetry.PprzMessage_GPS):
+            if ac_id in self.ac_ids:# and msg.name == "GPS":
+                ac = self.ac_dict[ac_id]
+                ac.gps = msg
+        self._ivy_interface.subscribe(gps_cb,telemetry.PprzMessage_GPS())
+
+        # bind to GVF message
+        def gvf_cb(ac_id, msg:telemetry.PprzMessage_GVF):
+            if ac_id in self.ac_ids:# and msg.name == "GVF":
+                ac = self.ac_dict[ac_id]
+                ac.gvf = msg
+                ac.gvf_parametric = None
+        self._ivy_interface.subscribe(gvf_cb, telemetry.PprzMessage_GVF())
+        
+        # bind to GVF_PARAMETRIC message
+        def gvf_par_cb(ac_id, msg:telemetry.PprzMessage_GVF_PARAMETRIC):
+            if ac_id in self.ac_ids:# and msg.name == "GVF_PARAMETRIC":
+                ac = self.ac_dict[ac_id]
+                ac.gvf_parametric = msg
+                ac.gvf = None
+        self._ivy_interface.subscribe(gvf_par_cb, telemetry.PprzMessage_GVF_PARAMETRIC())
