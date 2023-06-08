01b27e317d620ec76cff60fb92cc679197f35f64
Florian Sansou <fl.sansou@gmail.com>
2023-06-08 09:05:00 +0200

    Driver correction for amt22

--- sw/airborne/peripherals/amt22.c
+++ sw/airborne/peripherals/amt22.c
@@ -6,6 +6,19 @@
 #define AMT22_SPI_CDIV SPIDiv256
 #endif
 
+void short_sleep(struct spi_transaction *t) {
+  (void)t;
+  chThdSleepMicroseconds(4);
+}
+
+
+void amt_submit_byte(struct amt22_t *amt, uint8_t c) {
+  amt->trans.output_buf[0] = c;
+  amt->trans.output_length = 1;
+  amt->trans.input_length = 1;
+  spi_submit(amt->periph, &amt->trans);
+  amt->byte_no++;
+}
 
 void amt22_init(struct amt22_t *amt, struct spi_periph *periph, uint8_t slave_idx) {
   // Set up SPI peripheral and transaction
@@ -19,11 +32,12 @@ void amt22_init(struct amt22_t *amt, struct spi_periph *periph, uint8_t slave_id
   amt->trans.dss = SPIDss8bit;
   amt->trans.bitorder = SPIMSBFirst;
   amt->trans.cdiv = AMT22_SPI_CDIV;
-  amt->trans.before_cb = NULL;
-  amt->trans.after_cb = NULL;
+  amt->trans.before_cb = short_sleep;
+  amt->trans.after_cb = short_sleep;
   amt->trans.status = SPITransDone;
+  amt->byte_no = 0;
 
-  amt->request = AMT22_SET_ZERO;
+  amt->request = AMT22_READ_POSITION;
   amt->position = 0;
   amt->turns = 0;
   amt->offset = 0;
@@ -34,6 +48,7 @@ void amt22_init(struct amt22_t *amt, struct spi_periph *periph, uint8_t slave_id
 void amt22_request(struct amt22_t *amt, enum amt22_request_t request) {
   if(amt->trans.status == SPITransDone) {
     amt->request = request;
+    amt->byte_no = 0;       // reset byte number before starting the transaction.
   }
 }
 
@@ -51,76 +66,86 @@ void amt22_event(struct amt22_t *amt) {
 
   // spi success, read data
   if(amt->trans.status == SPITransSuccess) {
-    if(amt->request == AMT22_READ_POSITION || amt->request == AMT22_SET_ZERO) {
-      if(amt->request == AMT22_SET_ZERO){
-        amt->request = AMT22_READ_POSITION;
-      }
-      uint8_t p0 = amt->trans.input_buf[0];
-      uint8_t p1 = amt->trans.input_buf[1];
+    // we do the transaction one byte at a time, so store the data in a persistant buffer.
+    amt->persistant_buf[amt->byte_no-1] = amt->trans.input_buf[0];
+    amt->trans.status = SPITransDone;
+
+    if(amt->byte_no == 2 && (amt->request == AMT22_READ_POSITION || amt->request == AMT22_SET_ZERO)) {
+      uint8_t p0 = amt->persistant_buf[0];
+      uint8_t p1 = amt->persistant_buf[1];
       uint16_t data = p0 << 8 | p1;
       
       if(amt22_checkbit(data)){
         if(amt->flag_offset == 0){
-          amt->offset = (data & 0x3fff)>>2; //0x3fff -> 0011111111111111
+          amt->offset = (data & 0x3fff) >> 2;
           amt->flag_offset = 1;
         }
-        amt->position = ((data & 0x3fff)>>2) - amt->offset;
+        amt->position = ((data & 0x3fff) >> 2) - amt->offset; // 12 bits so shift 2
         amt->angle_rad = amt->position*2*M_PI/4096;
       }
-      else{
-        if(amt->flag_offset == 0){
-          amt->offset = ((data & 0x3fff)>>2) + 32; //0x3fff -> 0011111111111111
-          amt->flag_offset = 1;
-        }
-        amt->position = ((data & 0x3fff)>>2) + 32 - amt->offset; //offset de 32 probleme capteur 2^5 = 32
-        amt->angle_rad = amt->position*2*M_PI/4096;
-      }
-      
-      
-      
+      amt->request = AMT22_NONE;
     }
-    else if(amt->request == AMT22_READ_TURNS) {
-      uint8_t p0 = amt->trans.input_buf[0];
-      uint8_t p1 = amt->trans.input_buf[1];
-      uint8_t t0 = amt->trans.input_buf[2];
-      uint8_t t1 = amt->trans.input_buf[3];
+    else if(amt->byte_no == 4 && amt->request == AMT22_READ_TURNS) {
+      uint8_t p0 = amt->persistant_buf[0];
+      uint8_t p1 = amt->persistant_buf[1];
+      uint8_t t0 = amt->persistant_buf[2];
+      uint8_t t1 = amt->persistant_buf[3];
       uint16_t data = p0 << 8 | p1;
       
       if(amt22_checkbit(data)){
-        amt->position = (data & 0x3fff)>>2; 
+        amt->position = (data & 0x3fff) >> 2; // 12 bits so shift 2
       }
       amt->turns =    (t0 << 8 | t1);
-      
+      amt->request = AMT22_NONE;
     }
-
-    amt->trans.status = SPITransDone;
-    amt->request = AMT22_NONE;
   }
 
   // spi ready
   if(amt->trans.status == SPITransDone) {
     if(amt->request == AMT22_READ_POSITION) {
-      amt->trans.output_buf[0] = 0x00;
-      amt->trans.output_buf[1] = 0x00;
-      amt->trans.output_length = 2;
-      amt->trans.input_length = 2;
-      spi_submit(amt->periph, &amt->trans);
+      switch(amt->byte_no) {
+        case 0:
+          amt->trans.select = SPISelect;
+          amt_submit_byte(amt, 0x00);
+          break;
+        case 1:
+          amt->trans.select = SPIUnselect;
+          amt_submit_byte(amt, 0x00);
+          break;
+      }
     }
     if(amt->request == AMT22_SET_ZERO) {
-      amt->trans.output_buf[0] = 0x00;
-      amt->trans.output_buf[1] = 0x70;
-      amt->trans.output_length = 2;
-      amt->trans.input_length = 2;
-      spi_submit(amt->periph, &amt->trans);
+      switch(amt->byte_no) {
+        case 0:
+          amt->trans.select = SPISelect;
+          amt_submit_byte(amt, 0x00);
+          break;
+        case 1:
+          amt->trans.select = SPIUnselect;
+          amt_submit_byte(amt, 0x70);
+          break;
+      }
     }
     else if(amt->request == AMT22_READ_TURNS) {
-      amt->trans.output_buf[0] = 0x00;
-      amt->trans.output_buf[1] = 0xA0;
-      amt->trans.output_buf[2] = 0x00;
-      amt->trans.output_buf[3] = 0x00;
-      amt->trans.output_length = 4;
-      amt->trans.input_length = 4;
-      spi_submit(amt->periph, &amt->trans);
+      switch(amt->byte_no) {
+        case 0:
+          amt->trans.select = SPISelect;
+          amt_submit_byte(amt, 0x00);
+          break;
+        case 1:
+          amt->trans.select = SPINoSelect;
+          amt_submit_byte(amt, 0xA0);
+          break;
+        case 2:
+          amt->trans.select = SPINoSelect;
+          amt_submit_byte(amt, 0x00);
+          break;
+        case 3:
+          amt->trans.select = SPIUnselect;
+          amt_submit_byte(amt, 0x00);
+          break;
+      }
+
     }
     
   }
