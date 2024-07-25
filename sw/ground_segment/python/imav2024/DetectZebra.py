9ed2f04c4b7975764aa36daf65d0aa8a89e793cd
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2024-07-25 22:05:50 +0200

    add zebra detector, geo data disabled and may need to be repared

---
+++ sw/ground_segment/python/imav2024/DetectZebra.py
@@ -0,0 +1,97 @@
+import cv2
+import cv2 as cv
+import numpy as np
+
+def boxPoints(pts):
+        if int(cv2.__version__[0]) >= 3:
+                    return cv2.boxPoints(pts)
+        else:
+                    return cv2.cv.BoxPoints(pts)
+
+class ZebraDetector:
+
+    def __init__(self, hsv_th, size, aspect_ratio_th=0.2, area_th=0.5, size_th=0.2, color="Unknown"):
+        self.hsv_th = None
+        self.set_hsv_th(hsv_th[0], hsv_th[1])
+        self.size_min = min(size) # in meters
+        self.size_max = max(size) # in meters
+        self.aspect_ratio = self.size_max / self.size_min
+        self.area = self.size_max * self.size_min
+        #self.size2 = size * size * aspect_ratio # real size in mm
+        self.aspect_ratio_th = aspect_ratio_th
+        self.area_th = area_th
+        self.size_th = size_th
+        self.kernel = np.ones((8,8),np.uint8) # create convolution
+        self.mask = None
+        self.color = color # color name
+
+    def detect(self, img, resolution):
+
+        #blur = cv2.GaussianBlur(img,(5,5),0)
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = None
+        for th in self.hsv_th:
+            print('hsv th',th)
+            hsv_min = np.array(th[0])
+            hsv_max = np.array(th[1])
+            if mask is None:
+                mask = cv2.inRange(hsv, hsv_min, hsv_max)
+            else:
+                mask += cv2.inRange(hsv, hsv_min, hsv_max)
+
+        self.mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, self.kernel) # opening
+        cv2.imshow('mask '+self.color,cv2.resize(mask,(1024,720)))
+        #cv2.imshow('mask '+self.color,mask)
+        cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        self.draw_all(img.copy(),cnts)
+        valid = []
+        error = []
+        for cnt in cnts:
+            rect = cv2.minAreaRect(cnt)
+            _, (w, h), _ = rect
+            if w == 0 or h == 0:
+                continue
+            min_wh = min(w, h) / float(resolution)
+            max_wh = max(w, h) / float(resolution)
+            aspect_ratio = max_wh / min_wh
+            area = min_wh * max_wh
+            error_ar = min(1., abs(aspect_ratio - self.aspect_ratio) / self.aspect_ratio)
+            error_area = min(1., abs(area - self.area) / self.area)
+            error_size_min = abs(min_wh - self.size_min) / self.size_min
+            error_size_max = abs(max_wh - self.size_max) / self.size_max
+            area_ratio = cv2.contourArea(cnt) / (area * resolution * resolution)
+            score = area_ratio * (1. - error_area) * (1. - error_ar)
+            if error_size_min > self.size_th or error_size_max > self.size_th:
+                # too small or too big
+                #print("not correct size")
+                error.append((score, 'size', rect, (min_wh, max_wh, error_size_min, error_size_max, self.size_th)))
+            elif error_ar > self.aspect_ratio_th:
+                # not correct aspect ratio
+                #print("not correct aspect ratio")
+                error.append((score, 'aspect', rect, (error_ar, aspect_ratio, self.aspect_ratio, self.aspect_ratio_th)))
+            #print(area,cv2.contourArea(cnt),area_ratio)
+            elif area_ratio < self.area_th:
+                # not enough full of color
+                #print("not good filling")
+                error.append((score, 'filling', rect, (area_ratio, self.area_th)))
+            else:
+                valid.append((score, 'valid', rect, (min_wh, max_wh, error_ar, error_size_min, error_size_max, area_ratio)))
+        valid = sorted(valid, key=lambda k: k[0], reverse=True)
+        return valid, error
+
+    def draw_all(self, img, cnts):
+        for cnt in cnts:
+            box = boxPoints(cv2.minAreaRect(cnt))
+            ctr = np.array(box).reshape((-1,1,2)).astype(np.int32)
+            cv2.drawContours(img, [ctr], -1, (0, 255, 0), 4)
+        cv2.imshow('contour '+self.color,cv.resize(img,(1024,720)))
+
+    def set_hsv_th(self, th_min, th_max):
+        if th_min[0] < th_max[0]: # h min < h max, normal case
+            self.hsv_th = np.array([[th_min, th_max]])
+        else: # split into two parts
+            self.hsv_th = np.array([
+                [[0        , th_min[1], th_min[2]],[th_max[0], th_max[1], th_max[2]]],
+                [[th_min[0], th_min[1], th_min[2]],[179      , th_max[1], th_max[2]]]
+                ])
+
---
+++ sw/ground_segment/python/imav2024/Zebra_detector_2024.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+import cv2
+from DetectZebra import ZebraDetector
+import numpy as np
+import rasterio.warp
+from rasterio.crs import CRS
+
+def boxPoints(pts):
+    if int(cv2.__version__[0]) >= 3:
+        return cv2.boxPoints(pts)
+    else:
+        return cv2.cv.BoxPoints(pts)
+
+DEFAULT_IMAGE_VIEWER = "gwenview"
+DEFAULT_IMAGE_OUTPUT = "out_detect.png"
+DEFAULT_SCALE_FACTOR = 4
+DEFAULT_RESOLUTION = 20 # pixels per meter
+
+mailbox_zebra = ZebraDetector([[0, 0, 230],[179, 15, 255]], (1.7, 0.6), color="Zebra")
+
+
+def get_geo_data(filename):
+    import os
+    if os.path.splitext(filename)[1] == '.tif':
+        try:
+            import rasterio
+            # Open tif file
+            print(f"opening {filename}...")
+            ds = rasterio.open(filename)
+            return ds
+        except:
+            print('failed loading rasterio')
+            return None
+    else:
+        print('not a tif file')
+        return None
+
+def process_result(img, out, res, label, geo=None, color=(0, 255, 0)):
+    center = (int(res[0][0]), int(res[0][1]))
+    cv2.circle(out, center, 50, color, 5)
+    #cv2.putText(out, label, (center[0]+60, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), lineType=cv2.LINE_AA)
+    cv2.putText(out, label, (center[0]+60, center[1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0))
+
+    if geo is not None:
+        # get pixel coordinates
+        coord = geo.xy(center[0], center[1])
+        if coord is not None:
+            # transform to WGS84
+            lons, lats = rasterio.warp.transform(geo.crs, CRS.from_epsg(4326), [coord[0]], [coord[1]])
+            print(f"{label} {center} {lats[0]:.07f}, {lons[0]:.07f}")
+            cv2.putText(out, '{:.7f} {:.7f}'.format(lats[0], lons[0]), (center[0]+60, center[1]+30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, color)
+    return img, out
+
+def find_mailboxes(img, output=None, scale=DEFAULT_SCALE_FACTOR, res=DEFAULT_RESOLUTION, geo=None, show_errors=False):
+    out = img.copy()
+
+    results, error = mailbox_zebra.detect(img, res)
+    #print('results', len(results))
+    for cnt, result in enumerate(results):
+        print(f'ZEBRA_{cnt} | {result[0]} | {result[3]}')
+        img, out = process_result(img, out, result[2], f"ZEBRA_{cnt}", geo)
+    if show_errors:
+        print('number of errors', len(error))
+        for cnt, result in enumerate(error):
+            s = ' '.join(format(f, '.3f') for f in result[3])
+            img, out = process_result(img, out, result[2], f"{result[1]} | {s}", geo, color=(0, 0, 255))
+
+    if output is None:
+        w, h, _ = img.shape
+        img_out = cv2.resize(out, (int(h/scale),int(w/scale)))
+        cv2.imshow('frame',img_out)
+        while True:
+            if cv2.waitKey(-1)  & 0xFF == ord('q'):
+                break
+        cv2.destroyAllWindows()
+    else:
+        cv2.imwrite(output, out)
+
+if __name__ == '__main__':
+    '''
+    When used as a standalone script
+    '''
+    import argparse
+    import subprocess
+
+    parser = argparse.ArgumentParser(description="Search mailboxes in image")
+    parser.add_argument('img', help="image path")
+    parser.add_argument("-v", "--viewer", help="program used to open the image", default=DEFAULT_IMAGE_VIEWER)
+    parser.add_argument("-nv", "--no_view", help="Do not open image after processing", action='store_true')
+    parser.add_argument("-o", "--output", help="output file name", default=None)
+    parser.add_argument("-s", "--scale", help="resize scale factor", type=int, default=DEFAULT_SCALE_FACTOR)
+    parser.add_argument("-r", "--resolution", help="resolution in pixels per meter", type=float, default=DEFAULT_RESOLUTION)
+    parser.add_argument("-e", "--error", help="show invalid detection", type=bool, default=False)
+    args = parser.parse_args()
+
+    img = cv2.imread(args.img)
+    geo = None # get_geo_data(args.img)
+    find_mailboxes(img, args.output, args.scale, args.resolution, geo=geo, show_errors=args.error)
+
+    if not args.no_view and args.output is not None:
+        subprocess.call([args.viewer, args.output])
+
