0cd4209c9d78df5f3703851c01404451c757f70c
Mael FEURGARD <mael.feurgard@laas.fr>
2026-07-20 13:26:54 +0200

    Why does gcs_launch.py still exists ?

--- sw/tools/gcs_launch.py
+++ sw/tools/gcs_launch.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+##### WHY DOES THIS FILE EXIST ????? #####
+
 if __name__ == "__main__":
     print('Error: This wrapper has been removed. Please use directly the "PprzGCS" tool.')
     print('You are probably using a custom control_panel.xml. Remove the "PprzGCS" tool from the "programs" section of your control panel.')
