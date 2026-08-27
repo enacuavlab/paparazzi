009dacf9ecebc4ce3a1c5814fb3050bc28174861
Fabien-B <Fabien-B@github.com>
2026-08-27 18:23:14 +0200

    fix tawaki2

--- sw/airborne/boards/tawaki/chibios/v2.0/mcuconf_board.h
+++ sw/airborne/boards/tawaki/chibios/v2.0/mcuconf_board.h
@@ -428,9 +428,9 @@
 #define STM32_SERIAL_USE_USART3             FALSE
 #endif
 #if USE_UART4
-#define STM32_SERIAL_USE_USART4             TRUE
+#define STM32_SERIAL_USE_UART4             TRUE
 #else
-#define STM32_SERIAL_USE_USART4             FALSE
+#define STM32_SERIAL_USE_UART4             FALSE
 #endif
 #define STM32_SERIAL_USE_UART5              FALSE
 #define STM32_SERIAL_USE_USART6             FALSE
