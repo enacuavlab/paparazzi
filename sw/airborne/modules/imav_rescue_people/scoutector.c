b63be3a85eb1b66b7c26404d3a7f151945d1e9fd
Gautier Hattenberger <gautier.hattenberger@enac.fr>
2026-09-07 00:22:02 +0200

    add map to scout detector

--- sw/airborne/modules/imav_rescue_people/scoutector.c
+++ sw/airborne/modules/imav_rescue_people/scoutector.c
@@ -28,10 +28,19 @@
 #include "uavcan/uavcan.h"
 #include "uavcan.protocol.debug.KeyValue.h"
 #include "modules/datalink/downlink.h"
+#include "modules/nav/waypoints.h"
+#include <generated/flight_plan.h>
+
+// FIXME make a better config
+#ifndef WP_SEARCH
+#define WP_SEARCH 0
+#endif
 
 static uavcan_event scoutector_uavcan_ev;
 
 scoutector_t scout_data;
+
+struct scout_map_t scout_map;
 
 static void scoutector_uavcan_cb(struct uavcan_iface_t *iface __attribute__((unused)), CanardRxTransfer *transfer) {
   struct uavcan_protocol_debug_KeyValue msg;
@@ -57,12 +66,19 @@ static void scoutector_uavcan_cb(struct uavcan_iface_t *iface __attribute__((unu
   } else if(strncmp("ltu", (const char*)msg.key.data, 3) == 0) {
     //TODO log ?
   }
-  
+
+  // update map
+  scout_mat_update(&scout_map, scout_data, *stateGetPositionNed_f());
 }
 
 
 void scoutector_init(void)
 {
+  struct EnuCoor_f enu = *waypoint_get_enu_f(WP_SEARCH);
+  struct NedCoor_f ned;
+  ENU_OF_TO_NED(ned, enu);
+  scout_map_init(&scout_map, ned, 1.f);
+
   uavcan_bind(UAVCAN_PROTOCOL_DEBUG_KEYVALUE_ID, UAVCAN_PROTOCOL_DEBUG_KEYVALUE_SIGNATURE, &scoutector_uavcan_ev, &scoutector_uavcan_cb);
 }
 
@@ -70,3 +86,69 @@ void scoutector_report(void) {
   float f[3] = {scout_data.det, scout_data.snr, scout_data.lit};
   DOWNLINK_SEND_PAYLOAD_FLOAT(DefaultChannel, DefaultDevice, 3, f);
 }
+
+
+/*******
+ * MAP *
+ *******/
+
+void scout_map_init(struct scout_map_t *map, struct NedCoor_f pos, float res)
+{
+  map->res = res;
+  map->center = pos;
+  for (int i = 0; i < SCOUT_MAP_SIZE; i++) {
+    for (int j = 0; j < SCOUT_MAP_SIZE; j++) {
+      map->grid[i][j].det = 0.f;
+      map->grid[i][j].snr = 0.f;
+      map->grid[i][j].lit = 0.f;
+    }
+  }
+}
+
+bool scout_mat_update(struct scout_map_t *map, scoutector_t data, struct NedCoor_f pos)
+{
+  int x = (int)((pos.x - map->center.x) / map->res + SCOUT_MAP_SIZE / 2.f + 0.5f);
+  int y = (int)((pos.y - map->center.y) / map->res + SCOUT_MAP_SIZE / 2.f + 0.5f);
+  if (x < 0 || x >= SCOUT_MAP_SIZE || y < 0 || y >= SCOUT_MAP_SIZE) {
+    return false; // out of map
+  }
+  map->grid[x][y] = data; // TODO filter with current data in cell
+  return true;
+}
+
+float scout_mat_get_barycenter(struct scout_map_t *map, struct NedCoor_f *pos)
+{
+  float bx = 0.f;
+  float by = 0.f;
+  float quality = 0.f;
+  int nb_qual = 0;
+  float sum = 0.f;
+  const int offset = (int)(SCOUT_MAP_SIZE / 2.f + 0.5f);
+  for (int i = 0; i < SCOUT_MAP_SIZE; i++) {
+    for (int j = 0; j < SCOUT_MAP_SIZE; j++) {
+      bx += map->grid[i][j].snr * (float)(i - offset);
+      by += map->grid[i][j].snr * (float)(j - offset);
+      sum += map->grid[i][j].snr;
+      if (map->grid[i][j].snr > 0.f) {
+        quality += map->grid[i][j].snr;
+        nb_qual++;
+      }
+    }
+  }
+  pos->x = map->center.x + (bx * map->res / sum);
+  pos->y = map->center.y + (by * map->res / sum);
+  pos->z = map->center.z;
+  quality /= (float)nb_qual;
+  return quality;
+}
+
+void scout_map_update_wp(struct scout_map_t *map, uint8_t wp_id)
+{
+  struct NedCoor_f pos;
+  if (scout_mat_get_barycenter(map, &pos) > 0.f) {
+    struct EnuCoor_f enu;
+    ENU_OF_TO_NED(enu, pos);
+    waypoint_set_enu(wp_id, &enu);
+  }
+}
+
--- sw/airborne/modules/imav_rescue_people/scoutector.h
+++ sw/airborne/modules/imav_rescue_people/scoutector.h
@@ -26,6 +26,8 @@
 #ifndef SCOUTECTOR_H
 #define SCOUTECTOR_H
 
+#include "std.h"
+#include "math/pprz_geodetic_float.h"
 
 typedef struct {
   float det;
@@ -37,5 +39,47 @@ extern void scoutector_init(void);
 extern void scoutector_report(void);
 
 extern scoutector_t scout_data;
+
+#ifndef SCOUT_MAP_SIZE
+#define SCOUT_MAP_SIZE 51
+#endif
+
+/** map of scout detection
+ */
+struct scout_map_t {
+  scoutector_t grid[SCOUT_MAP_SIZE][SCOUT_MAP_SIZE];
+  struct NedCoor_f center;                            ///< center of the map
+  float res;                                          ///< map resolution in m/cell
+};
+
+extern struct scout_map_t scout_map;
+
+/** init map
+ * @param[in] map pointer to map
+ * @param[in] pos position of the map center
+ * @param[in] res resolution of the map in m per cell
+ */
+extern void scout_map_init(struct scout_map_t *map, struct NedCoor_f pos, float res);
+
+/** update map with new data
+ * @param[in] map pointer to map
+ * @param[in] data new data
+ * @param[in] pos position of the new data
+ * @return false is position is outside the map
+ */
+extern bool scout_mat_update(struct scout_map_t *map, scoutector_t data, struct NedCoor_f pos);
+
+/** get weighted center of the measurements (snr), expected to be the scout position
+ * @param[in] map pointer to map
+ * @param[out] pos pointer to computed position
+ * @return signal quality (the averaged snr level FIXME really needed ?)
+ */
+extern float scout_mat_get_barycenter(struct scout_map_t *map, struct NedCoor_f *pos);
+
+/** update waypoint position from map
+ * @param[in] map pointer to map
+ * @param[in] wp_id waypoint ID
+ */
+extern void scout_map_update_wp(struct scout_map_t *map, uint8_t wp_id);
 
 #endif  // SCOUTECTOR_H
